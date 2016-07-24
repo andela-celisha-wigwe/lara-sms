2f81e9711e76cc3bac3fc4417862fa5237e95d42
andela-celisha-wigwe <chijioke.elisha-wigwe@andela.com>
2016-07-25 00:25:07 +0100

    create new develop branch

---
+++ src/SMSManager.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LARASMS\SMSManager;
+
+class SMSManager
+{
+    public function __construct()
+    {
+        // Set the configuration.
+    }
+
+    public function send()
+    {
+        // send the SMS.
+    }
+}
